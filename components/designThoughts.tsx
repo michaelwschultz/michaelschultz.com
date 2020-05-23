639bff5bcc75de1cb854545d94a774099356e227
Michael Schultz <michaelwschultz@gmail.com>
2020-05-23 16:34:27 -0700

    Adds loading prop to simplecast iFrame

--- components/designThoughts.tsx
+++ components/designThoughts.tsx
@@ -5,7 +5,16 @@ const DesignThoughts = () => {
   return (
     <section className="mw8 center mv6">
       <h3 className="white f5 pb3">Thoughts on design</h3>
-      <ReactPlayer url="https://embed.simplecast.com/c6d6f4c3" width="100%" />
+      <iframe
+        src='https://embed.simplecast.com/c6d6f4c3'
+        width='100%'
+        frameBorder='0'
+        height='200px'
+        scrolling='no'
+        seamless
+        loading="lazy"
+        >
+      </iframe>
     </section>
   )
 }
