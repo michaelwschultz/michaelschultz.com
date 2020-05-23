27b9f08e3143117495d2d6145d066e0e8bfd9936
Michael Schultz <michaelwschultz@gmail.com>
2020-05-23 16:54:28 -0700

    adds react-lazy for loading 3rd party players

--- components/designThoughts.tsx
+++ components/designThoughts.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import { Lazy } from 'react-lazy'
 
 const DesignThoughts = (): JSX.Element => {
   return (
     <section className="mw8 center mv6">
       <h3 className="white f5 pb3">Thoughts on design</h3>
-      <iframe
-        src='https://embed.simplecast.com/c6d6f4c3'
-        width='100%'
-        frameBorder='0'
-        height='200px'
-        scrolling='no'
-        seamless
-        data-loading="lazy"
-      />
+      <Lazy>
+        <iframe
+          src='https://embed.simplecast.com/c6d6f4c3'
+          width='100%'
+          frameBorder='0'
+          height='200px'
+          scrolling='no'
+          seamless
+        />
+      </Lazy>
     </section>
   )
 }
