0a4f6b566de94a1a82b01c3acf5c5885f23a888b
Michael Schultz <michaelwschultz@gmail.com>
2020-06-05 12:01:48 -0700

    Removes unused css and fixes favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,8 @@ const Home = () => {
     <Page>
       <Head>
         <title>Michael Schultz | Product Designer & Engineer</title>
+        <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon-16x16.png" />
       </Head>
 
       <Intro />
