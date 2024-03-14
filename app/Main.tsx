88dee904a8172f201fc64770d1c9f614ff4c3469
Michael Schultz <michaelwschultz@gmail.com>
2024-03-13 21:51:42 -0700

    WIP - Adds project cards and content

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,6 +3,7 @@ import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
+import ProjectCard from '@/components/ProjectCard'
 
 const MAX_DISPLAY = 1
 
@@ -26,13 +27,58 @@ export default function Home({ posts, heroFontStyles }) {
           {`Software designer and engineer \nwith a passion for creating— \nfrom innovative web \nsolutions to video games, \nmusic, and impactful \nsoftware projects.`}
         </p>
       </div>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+
+      {/* PROJECTS - move to component */}
+      <div className="flex flex-wrap gap-4 pt-24">
+        <ProjectCard
+          title="Hemolog 2.0"
+          href="https://hemolog.com"
+          buttonText="Try Now"
+          buttonColor="bg-green-800 hover:bg-green-700"
+        >
+          A free health tracking tool for people with Hemophilia, rebuilt from the ground up to be
+          simple and more insightful than ever.
+        </ProjectCard>
+        <ProjectCard title="(Ph)one Stand" buttonText="Available Soon">
+          Tired of not having a quality phone stand that rotated nicely with one hand, I decided to
+          design my own. Made with strong, recyclable plastics and eco-friendly Jesmonite.
+        </ProjectCard>
+        <ProjectCard
+          title="Pack Rat"
+          href="https://tokoroko.itch.io/pack-rat"
+          buttonText="Coming Soon"
+          buttonColor="bg-green-800 hover:bg-green-700"
+        >
+          A point-and-click adventure built for the Cozy Autumn Game Jam 2023.
+          <span className="block pt-4">
+            The game was designed and built in 8 days and lots of coffee. This was also the first
+            ever game published by <Link href="https://ratrace.studio">RatRace.studio</Link>
+          </span>
+        </ProjectCard>
+        <ProjectCard title="Time Between The Stars" buttonText="Coming Soon">
+          Space simulation game influenced by the short story{' '}
+          <Link href="https://www.amazon.com/Slow-Between-Stars-Reaches-collection-ebook/dp/B0C4QX2FSF">
+            Slow Time Between The Stars by John Scalzi
+          </Link>
+          .
+        </ProjectCard>
+        <ProjectCard title="Toko Roko" buttonText="Coming Soon">
+          Hand drawn parkour platformer for PC and consoles.
+          <span className="block pt-4">
+            This will be the first official game from{' '}
+            <Link href="https://ratrace.studio">RatRace.studio</Link> a collaboration between me and
+            Luke Schultz.
+          </span>
+        </ProjectCard>
+      </div>
+
+      <div className="divide-y divide-gray-200 pt-20 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:leading-14">
             Latest thoughts
           </h1>
         </div>
-        {/* Posts */}
+        {/* POSTS */}
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
--- components/Link.tsx
+++ components/Link.tsx
@@ -4,8 +4,8 @@ import type { LinkProps } from 'next/link'
 import { AnchorHTMLAttributes } from 'react'
 
 const CustomLink = ({ href, ...rest }: LinkProps & AnchorHTMLAttributes<HTMLAnchorElement>) => {
-  const isInternalLink = href && href.startsWith('/')
-  const isAnchorLink = href && href.startsWith('#')
+  const isInternalLink = href?.startsWith('/')
+  const isAnchorLink = href?.startsWith('#')
 
   if (isInternalLink) {
     return <Link href={href} {...rest} />
