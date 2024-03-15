c364339efe93142898ff8a66072f47a0b69f2ef3
Michael Schultz <michaelwschultz@gmail.com>
2024-03-15 02:19:00 -0700

    Adds priority to logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <div className="flex items-center justify-between">
       <div className="mr-1">
-        <Image src="/static/images/ms.svg" alt="logo" width="31" height="24" />
+        <Image src="/static/images/ms.svg" alt="logo" width="31" height="24" priority />
       </div>
     </div>
   )
