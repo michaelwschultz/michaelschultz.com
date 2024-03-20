9c75ae87f4a38b8ca17ae8afd612607a45b1fa7e
Michael Schultz <michaelwschultz@gmail.com>
2024-03-19 23:23:56 -0700

    Adds tag pages back and places them under thoughts directory

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -105,7 +105,7 @@ export default function ListLayoutWithTags({
                       </h3>
                     ) : (
                       <Link
-                        href={`/tags/${slug(t)}`}
+                        href={`/thoughts/tags/${slug(t)}`}
                         className="px-3 py-2 text-sm font-medium uppercase text-gray-500 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
                         aria-label={`View posts tagged ${t}`}
                       >
