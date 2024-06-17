ec6f2a4185b0c88e037272f2dba606f246c61ada
Michael Schultz <michaelwschultz@gmail.com>
2024-06-16 20:10:10 -0700

    Adds tracking via umami.is

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -21,7 +21,8 @@ const siteMetadata = {
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
     umamiAnalytics: {
       // We use an env variable for this site to avoid other users cloning our analytics ID
-      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+      umamiWebsiteId: process.env.UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+      src: 'https://analytics.us.umami.is/script.js',
       // You may also need to overwrite the script if you're storing data in the US - ex:
       // src: 'https://us.umami.is/script.js'
       // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
