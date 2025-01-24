15d04f4547972878d7f96401bede401bfa14316f
Michael Schultz <michaelwschultz@gmail.com>
2025-01-23 21:20:37 -0800

    adds green bg colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,12 +8,12 @@ module.exports = {
 		current: "currentColor",
 		extend: {
 			colors: {
-				primary: "#0E0E0E",
+				primary: "#001807",
 				secondary: "#ECECEC",
 				secondaryDark: "#D8D8D8",
 				secondaryDarker: "#6C7072",
 				socialBorder: "#4B4E4F",
-				hoverBackground: "#1C1C1C",
+				hoverBackground: "#082912",
 			},
 		},
 	},
