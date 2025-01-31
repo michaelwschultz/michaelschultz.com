99d5ee7ec9b0aebae0a7714649c3bda2bcecdd51
Michael Schultz <michaelwschultz@gmail.com>
2025-01-31 00:59:54 -0800

    Adds bluesky social icon to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,13 @@
-import Link from './Link'
-import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
+import siteMetadata from '@/data/siteMetadata'
+import Link from './Link'
 
 export default function Footer() {
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
         <div className="mb-3 flex space-x-4">
+          <SocialIcon kind="bluesky" href={siteMetadata.bluesky} size={6} />
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
