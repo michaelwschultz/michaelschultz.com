6b1b7a6909393f2009a0b8a1e67370d8aece1699
Michael Schultz <michaelwschultz@gmail.com>
2025-01-30 23:46:49 -0800

    reorder imports

--- app/thoughts/page/[page]/page.tsx
+++ app/thoughts/page/[page]/page.tsx
@@ -1,6 +1,6 @@
 import ListLayout from '@/layouts/ListLayoutWithTags'
-import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allThoughts } from 'contentlayer/generated'
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 
 const POSTS_PER_PAGE = 5
 
